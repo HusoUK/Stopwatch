201643c7208eae22a6c0c4d38d062eeeb2cde50d
Richard Wilkinson <richwilk.iphone@gmail.com>
2022-11-14 12:06:45 +0000

    Initial Code Commit
    Added the console app project files.

---
+++ StopWatch/Exercise1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Exercise1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var stopWatch = new StopWatch();
+
+        stopWatch.Start();
+
+    }
+}
---
+++ StopWatch/Exercise1/StopWatch.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    internal class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private TimeSpan _duration;
+        private bool _hasAlreadyStarted = false;
+
+        public DateTime StartTime { get => _startTime; set => _startTime = value; }
+        public DateTime EndTime { get => _endTime; set => _endTime = value; }
+        public double Duration
+        {
+            get
+            {
+                var timeSpan = _endTime - StartTime;
+                double result = timeSpan.TotalSeconds;
+                return result;
+            }
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Please press enter to start the stopwatch:");
+            Console.ReadLine();
+            if (_hasAlreadyStarted == true)
+            {
+                throw new InvalidOperationException("the stop watch has already been started");
+            }
+            _startTime= DateTime.Now;
+            _hasAlreadyStarted= true;
+            Stop();
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Please press enter to stop the stopwatch:");
+            Console.ReadLine();
+            _endTime = DateTime.Now;
+            _hasAlreadyStarted= false;
+            Console.WriteLine($"The time this stop watch took was {Duration} seconds.");
+            Repeat();
+        }
+
+        public void Repeat()
+        {
+            Console.WriteLine("Would you like to run the stopwatch again ( Y / N )");
+            var repeating = Console.ReadLine();
+            switch (repeating)
+            {
+                case "Y" :
+                    Start();
+                    break;
+                case "N" :
+                    Console.WriteLine("The stopwatch program has ended, thank you.");
+                    break;
+                default:
+                    Console.WriteLine("please enter either Y or N (must be a capital).");
+                    Repeat();
+                    break;
+            }
+        }
+    }
+}
